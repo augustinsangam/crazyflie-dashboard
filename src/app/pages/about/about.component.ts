6ce6ec6ec8b81c77dfcddedeca97a0b214d82c1e
Eya-Tom Augustin SANGAM <augustin.sangam@gmail.com>
2021-01-29 14:26:06 -0500

    Add about page markdown

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import * as marked from 'marked';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'about-page',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 // eslint-disable-next-line @angular-eslint/component-class-suffix
-export class AboutPage { }
+export class AboutPage implements OnInit {
+  html: SafeHtml;
+
+  constructor(
+    private readonly sanitizer: DomSanitizer,
+    private readonly http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.http.get('/assets/about.md', { responseType: 'text' }).subscribe((data) => {
+      this.html = this.sanitizer.bypassSecurityTrustHtml(marked(data));
+    });
+  }
+}
---
+++ src/assets/about.md
@@ -0,0 +1,8 @@
+# Help
+Coming soon : Help page
+
+# About the page
+This page is developed and maintained by Équipe 202 :
+* Sangam, Eya-Tom Augustin
+* Grootenboer, Hubert
+* ...
