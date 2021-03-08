44e48e56abccca76d21be5584c38a01856e3a067
Eya-Tom Augustin SANGAM <eya-tom-augustin.sangam@polymtl.ca>
2021-03-07 20:39:59 -0500

    Add sensors in dashboard

--- src/app/models/robot.ts
+++ src/app/models/robot.ts
@@ -2,9 +2,10 @@ export interface Robot {
   name: string;
   timestamp: number;
   speed: number;
-  battery?: number;
-  position?: [number, number, number];
-  flying?: boolean;
+  battery: number;
+  position: [number, number, number];
+  multiRange?: [number, number, number, number, number];
+  flying: boolean;
   ledOn: boolean;
   real: boolean;
 }
@@ -13,6 +14,8 @@ export const DEFAULT_ROBOT: Robot = {
   name: `Fly # ${Math.ceil(Math.random() * 100)}`,
   timestamp: Date.now() / 1000,
   speed: 0,
+  battery: 50,
+  position: [0, 0, 0],
   flying: false,
   ledOn: false,
   real: false
